3e6d8ce408dbafd22c255a24773cb41399ede183
demesameneshoa <demesameneshoa@gmail.com>
2024-01-16 14:24:13 +0300

    Add components and views for react

---
+++ app/javascript/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Greeting from './Greeting';
+
+const App = () => {
+  return (
+      <Greeting />
+  );
+};
+
+export default App;
